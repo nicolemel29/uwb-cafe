6e820e199fa1d9c2a492eeea635602bfcf8f0e63
Antonymous1337 <45654017+Antonymous1337@users.noreply.github.com>
2024-12-03 21:29:18 -0800

    Edit Payment Page

--- src/PaymentView.js
+++ src/PaymentView.js
@@ -3,10 +3,37 @@ import './PaymentView.css'
 import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 
+
 function PaymentView() {
     const navigate = useNavigate()
 
     const [cart, setCart] = useState([])
+
+    function removeFromCart(index) {
+        const iterations = cart.length
+        let cartCopy = []
+        for (let i = 0; i < iterations; i++) {
+            if (i != index) {
+                cartCopy.push(cart[i])
+            }
+        }
+        setCart(cartCopy)
+    }
+
+    function subtractQuantity(cartItem, index) {
+        cartItem.quantity -= 1
+        if (cartItem.quantity <= 0) removeFromCart(index)
+        else setCart([...cart])
+    }
+
+    function addQuantity(cartItem) {
+        cartItem.quantity += 1
+        setCart([...cart])
+    }
+
+    function saveCart() {
+        localStorage.setItem("cart", JSON.stringify(cart))
+    }
 
     useEffect(() => {
         if (!localStorage.getItem("cart")) {
@@ -42,11 +69,41 @@ function PaymentView() {
             </head>
             <body>
                 <h1 id="order-header">Review Order</h1>
-                {
-                    cart.map(cartItem => (
-                        <p class="cart-item">{cartItem.itemName}</p>
-                    ))
-                }
+                <div id="order-container">
+                    <div class="content-container" id="order-info">
+                        <input type="text"></input>
+                    </div>
+                    <div class="content-container" id="order-recap">
+                        {cart.map((cartItem, index) => (
+                            <div class="cart-item">
+                                <span class="cart-item-title" key={`cart${index}`}>{cartItem.item.itemName}</span>
+                                <div class="cart-item-details">
+                                    <div class="cart-item-info">
+                                        <span class="cart-item-price">{`$${cartItem.item.price} each`}</span>
+                                        <span class="cart-item-quantity">{`Quantity: ${cartItem.quantity}`}</span>
+                                    </div>
+                                    <div class="cart-item-options">
+                                        <button onClick={() => {
+                                            removeFromCart(index)
+                                            saveCart()
+                                        }}>Delete</button>
+                                        <div class="cart-quantity-change">
+                                            <button onClick={() => {
+                                                subtractQuantity(cartItem, index)
+                                                saveCart();
+                                            }}>-</button>
+
+                                            <button onClick={() => {
+                                                addQuantity(cartItem)
+                                                saveCart();
+                                            }}>+</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
                 <button onClick={handlePay} class="pay-button">
                     Pay!
                 </button>
